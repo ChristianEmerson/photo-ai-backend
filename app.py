970fe49e1b73fe3125ad39187b6d8eb1156a3269
Chris ‎Stone <ChristianEmerson>
2025-06-24 19:47:43 +0530

    Initial commit for background remover

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/remove-bg', methods=['POST'])
+def remove_bg():
+    file = request.files['image']
+    input_image = Image.open(file.stream).convert("RGBA")
+    output = remove(input_image)
+    byte_io = io.BytesIO()
+    output.save(byte_io, format='PNG')
+    byte_io.seek(0)
+    return send_file(byte_io, mimetype='image/png')
+
+@app.route('/')
+def home():
+    return 'Background Remover is live!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
